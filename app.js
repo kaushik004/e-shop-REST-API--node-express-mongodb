0351b1092052b93ab11aafd36005ec33a99deeda
kaushik <kaushik@gmail.com>
2021-01-19 00:26:15 +0530

    Conneced to MongoDB database

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 require('dotenv/config');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const api = process.env.API_URL;
+
+// mongodb connection
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('Database is connected successfully....');
+})
+.catch((err) => {
+    console.log(err);
+})
 
 // Body Parser
 app.use(express.json());
