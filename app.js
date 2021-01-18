8335d82bcda1c09ce8d32109acac57d3bb57e40f
kaushik <kaushik@gmail.com>
2021-01-18 23:15:36 +0530

    Adde .env file and use morgan

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express');
+require('dotenv/config');
+const morgan = require('morgan');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const api = process.env.API_URL;
 
-app.get('/', (req, res) => {
-    res.send('Hello Api !!');
+// Body Parser
+app.use(express.json());
+// Morgan for logging
+app.use(morgan('tiny'));
+
+// http://127.0.0.1/api/v1/products
+app.get(`${api}/products`, (req, res) => {
+    const product = {
+        id: 1,
+        name: 'hair dresser',
+        image: 'url_url'
+    }
+    res.send(product);
+});
+
+app.post(`${api}/products`, (req, res) => {
+    const newProduct = req.body
+    console.log(newProduct);
+    res.send(newProduct);
 });
 
 app.listen(PORT, () => {
